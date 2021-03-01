4ced95a7151bdac3f8e98ebaac825024e919d0a3
hanool <29415530+hanool@users.noreply.github.com>
2021-03-02 06:31:05 +0900

    add test page for graph

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'pages/monthly_static_page.dart';
 
 void main() {
-  return runApp(MaterialApp(
-    home: Scaffold(
-      body: Text("My App"),
-    ),
-  ));
+  return runApp(AppHome());
 }
 
+class AppHome extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return PointsLineChart.withSampleData();
+  } 
+}
